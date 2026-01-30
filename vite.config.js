a08e260933f84fec11fa36051a7902d93e19dae4
DevelopingPontus <ipont35@gmail.com>
2026-01-30 10:38:44 +0100

    Deleted dev-dist folder, should be gitingored

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 import { VitePWA } from "vite-plugin-pwa";
 
-
 export default defineConfig(() => ({
     plugins: [
         eslint(),
@@ -18,16 +17,16 @@ export default defineConfig(() => ({
                 background_color: "#ffffff",
                 icons: [
                     { src: "./public/vite.svg", sizes: "192x192", type: "image/png" },
-                    { src: "./public/vite.svg", sizes: "512x512", type: "image/png" }
+                    { src: "./public/vite.svg", sizes: "512x512", type: "image/png" },
                 ],
-            }
-        })
+            },
+        }),
     ],
     build: {
         rollupOptions: {
             input: {
-                main: "index.html"
-            }
-        }
-    }
+                main: "index.html",
+            },
+        },
+    },
 }));
