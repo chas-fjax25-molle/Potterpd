9a2e193897d32e7625bc661fe50a9ceb3a9c4e6e
DevelopingPontus <ipont35@gmail.com>
2026-02-06 15:23:34 +0100

    Added som comments

--- src/RequestsFromAPI.js
+++ src/RequestsFromAPI.js
@@ -1,7 +1,11 @@
 /**
+ * @file Functions for fetching data from the API.
+ */
+
+/**
  * @param {string} category
  * @param {string} pageNumber - The page number to fetch. Defaults to 1. 50 is the default number of results per page.
- * @returns {Promise<*>}
+ * @returns {Promise<*>} - Category objects 50 results per page.
  */
 export async function getCategory(category, pageNumber) {
     const defaultPage = 1;
@@ -28,7 +32,7 @@ console.log(getCategory("characters", "1"));
 /**
  * @param {string} category
  * @param {string} id
- * @returns {Promise<*>}
+ * @returns {Promise<*>} - Specific object in category by id
  */
 export async function getSpecific(category, id) {
     const specificUrl = `https://api.potterdb.com/v1/${category}/${id}`;
@@ -55,6 +59,7 @@ console.log(getSpecific("books", "3e161309-4c3c-4094-9db9-a8dfe911de14"));
  * @param {string} filterParameter 
  * @param {string} filterValue
  * @param {string} pageNumber 
+ * @returns {Promise<*>} - Category objects filtered by filterParameter and filterValue, 50 results per page.
  */
 export async function getCategoryFilteredBy(category, filterParameter, filterValue, pageNumber) {
     const defaultPage = 1;
@@ -80,14 +85,15 @@ console.log(getCategoryFilteredBy("characters", "name_cont", "Potter", "1"));
 
 /**
  * 
- * @param {string} filterValue - Takes a string value and searches in all the fields of the API
+ * @param {string} searchValue - Takes a string value and searches in all the fields of the API
  * @param {string} pageNumber 
+ * @returns {Promise<*>} - Objects found by searching all objects for searchValue
  */
-export async function getSearchBy(filterValue, pageNumber) {
+export async function getSearchBy(searchValue, pageNumber) {
     const defaultPage = 1;
     const pageNumberValue = pageNumber || defaultPage;
 
-    const filterUrl = `https://api.potterdb.com/v1/?filter[in_any]=${filterValue}&?page[number]=${pageNumberValue}`;
+    const filterUrl = `https://api.potterdb.com/v1/?filter[in_any]=${searchValue}&?page[number]=${pageNumberValue}`;
 
     try {
         const response = await fetch(filterUrl);
