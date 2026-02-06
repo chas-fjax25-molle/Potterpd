d71a5db14def982b43612ad93e1e3079a08bb264
DevelopingPontus <ipont35@gmail.com>
2026-02-06 15:17:49 +0100

    Added getCategoryFilteredBy and GetSearchBy

--- src/RequestsFromAPI.js
+++ src/RequestsFromAPI.js
@@ -1,9 +1,13 @@
 /**
  * @param {string} category
+ * @param {string} pageNumber - The page number to fetch. Defaults to 1. 50 is the default number of results per page.
  * @returns {Promise<*>}
  */
-export async function getCategory(category) {
-    const categoryUrl = `https://api.potterdb.com/v1/${category}`;
+export async function getCategory(category, pageNumber) {
+    const defaultPage = 1;
+    const pageNumberValue = pageNumber || defaultPage;
+
+    const categoryUrl = `https://api.potterdb.com/v1/${category}?page[number]=${pageNumberValue}`;
 
     try {
         const response = await fetch(categoryUrl);
@@ -18,8 +22,8 @@ export async function getCategory(category) {
     }
 }
 
-console.log(getCategory("characters"));
-
+console.log(getCategory("characters", "2"));
+console.log(getCategory("characters", "1"));
 
 /**
  * @param {string} category
@@ -42,5 +46,60 @@ export async function getSpecific(category, id) {
     }
 }
 
-
 console.log(getSpecific("books", "3e161309-4c3c-4094-9db9-a8dfe911de14"));
+
+
+/**
+ * 
+ * @param {string} category 
+ * @param {string} filterParameter 
+ * @param {string} filterValue
+ * @param {string} pageNumber 
+ */
+export async function getCategoryFilteredBy(category, filterParameter, filterValue, pageNumber) {
+    const defaultPage = 1;
+    const pageNumberValue = pageNumber || defaultPage;
+
+    const filterUrl = `https://api.potterdb.com/v1/${category}?filter[${filterParameter}]=${filterValue}&?page[number]=${pageNumberValue}`;
+
+    try {
+        const response = await fetch(filterUrl);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+console.log(getCategoryFilteredBy("characters", "name_cont", "Potter", "1"));
+
+
+/**
+ * 
+ * @param {string} filterValue - Takes a string value and searches in all the fields of the API
+ * @param {string} pageNumber 
+ */
+export async function getSearchBy(filterValue, pageNumber) {
+    const defaultPage = 1;
+    const pageNumberValue = pageNumber || defaultPage;
+
+    const filterUrl = `https://api.potterdb.com/v1/?filter[in_any]=${filterValue}&?page[number]=${pageNumberValue}`;
+
+    try {
+        const response = await fetch(filterUrl);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+console.log(getSearchBy("Phoenix", "1"));
