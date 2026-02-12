efca15e25e6ead40dc8022f5d767839f1b4f88e0
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-02-12 14:57:01 +0100

    Basic listing of spells

---
+++ src/spell.js
@@ -0,0 +1,97 @@
+/**
+ * @file spell.js Spell class definition and HTML generation.
+ */
+
+import { favoriteIcon } from "./favorite_icon";
+
+/**
+ * CSS styles used in the Spell entity.
+ * @constant {Object} CSS - CSS styles for the Spell entity.
+ */
+const CSS = Object.freeze({
+    // Preview card classes
+    PREVIEW_CARD_CLASS: "spell-preview-card",
+    PREVIEW_NAME_CLASS: "spell-preview-name",
+    PREVIEW_IMAGE_CONTAINER_CLASS: "spell-preview-image-container",
+    PREVIEW_IMAGE_CLASS: "spell-preview-image",
+    PREVIEW_PLACEHOLDER_CLASS: "spell-preview-placeholder",
+});
+
+/**
+ * @typedef {Object} SpellAttributes
+ * @property {string} name - Name of the spell
+ * @property {string} incantation - Incantation of the spell
+ */
+
+/**
+ * @typedef {Object} SpellJSON
+ * @property {string} id - Unique identifier for the spell
+ * @property {string} type - Type of the object (should be "spells")
+ * @property {SpellAttributes} attributes - Attributes of the spell
+ */
+
+export class Spell {
+    /**
+     * @type {string} id - Unique identifier for the spell
+     */
+    id = "";
+
+    /**
+     * @type {string} type - Type of the object (should be "spells")
+     */
+    type = "spell";
+
+    /**
+     * @type {string} name - Name of the spell
+     */
+    name = "";
+
+    /**
+     * @type {string} incantation - Incantation of the spell
+     */
+    incantation = "";
+
+    /**
+     *
+     * @param {SpellJSON} json - JSON object representing a spell
+     * @returns {Spell} - An instance of the Spell class populated with data from the JSON object
+     *
+     * @throws {Error} Throws an error if the JSON object is missing required properties or if the properties are of incorrect types
+     */
+    static fromJson(json) {
+        const spell = new Spell();
+        if (!json.id || !json.type) {
+            throw new Error("Invalid JSON: Missing required properties 'id' or 'type'");
+        }
+        if (json.type !== "spell") {
+            throw new Error(`Invalid JSON: Expected type 'spell' but got '${json.type}'`);
+        }
+
+        spell.id = json.id || "";
+        spell.type = json.type || "spell";
+
+        if (!json.attributes) {
+            throw new Error("Invalid JSON: Missing 'attributes' properties");
+        }
+        /** @type {SpellAttributes} */
+        const attributes = json.attributes;
+
+        spell.name = attributes.name || "";
+        spell.incantation = attributes.incantation || "";
+
+        return spell;
+    }
+
+    previewHTML() {
+        const container = document.createElement("section");
+        container.classList.add(CSS.PREVIEW_CARD_CLASS);
+        container.dataset.spellId = this.id;
+        const header = document.createElement("h3");
+        header.classList.add(CSS.PREVIEW_NAME_CLASS);
+        header.textContent = this.name;
+        container.appendChild(header);
+        container.appendChild(favoriteIcon(this.id));
+
+        return container;
+    }
+}
